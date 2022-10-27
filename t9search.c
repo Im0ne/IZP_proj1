c81e023950ff1928034ce104b54e8820fcd74e82
Im0ne <116801019+Im0ne@users.noreply.github.com>
2022-10-27 17:00:48 +0200

    Update t9search.c

--- t9search.c
+++ t9search.c
@@ -7,32 +7,32 @@ int main(int argc, char *argv[])
 {
   const int count = 100; // maximum count of contacts
   size_t n = 0; // variable that depends of real number of contacts
-  int result = 0; // variable for check about state of input or output
+  int result = 1; // variable for check about state of input or output
   char lettonum[10][20] = {{"abcABC"}, {"defDEF"}, {"ghiGHI"}, {"jklJKL"}, 
   {"mnoMNO"}, {"pqrsPQRS"}, {"tuvTUV"}, {"wxyzWXYZ"}, {"+"}};             // 2d array of letters to transform
   char list[count][101]; //contacts                                                                                                              
   char arr[count][101];  //array for transformed contacts                                                                                                              
-  
-  if(argc>2||argc<1){
+
+  if(argc>2){
     result=2;
   }
-  if(result!=2){
-  while (n < count && fgets(list[n], sizeof(*list), stdin) != NULL){
-    ++n; // getting the contacts from .txt file
-  }
-  }
+
   if (argv[1] != NULL){
   for(int j = 0; j<strlen(argv[1]);j++) //checking if input only in numbers
   {
-
     if(argv[1][j]<48||argv[1][j]>57||argv[2]!=NULL){ 
       result=2;
-      
+
     }
   }
   }
 
-  if(result==0){ //checking if all ok with input
+  if(result==1){ //checking if all ok with input
+  
+  while (n < count && fgets(list[n], sizeof(*list), stdin) != NULL){
+    ++n; // getting the contacts from .txt file
+  }
+  
   for (size_t i = 0; i < n ; i++) 
   {
     list[i][strcspn(list[i], "\n")] = 0; // deleting the new line character from 2d array of contacts
@@ -43,13 +43,13 @@ int main(int argc, char *argv[])
     { 
       printf("%s, %s", list[i], list[i + 1]);
       i++;
-      result = 1;
+      result = 0;
     }
 
     else if (strstr(list[i], argv[1]) != NULL) // trying to find contact depending on number of contact
     { 
       printf("%s, %s\n", list[i - 1], list[i]);
-      result = 1; 
+      result = 0; 
     }
     
     else if (strncmp(list[i], argv[1], strlen(argv[1])) != 0)// trying to find contact depending on name of contact
@@ -105,20 +105,20 @@ int main(int argc, char *argv[])
       { 
         if(arr[i][0]=='0'){ //checking if list of contacts was number thats starts with +
           printf("%s, %s\n", list[i-1], list[i]);
-          result = 1;
+          result = 0;
         }
         else{
         printf("%s, %s\n", list[i], list[i + 1]);
-        result = 1;
+        result = 0;
         }
         }
     }
   }
   }
-  if (result == 0)
+  if (result == 1)
   {
     printf("Not found"); // output for not finding any contacts
-    
+    result=0;
   }
   else if (result == 2)
   {
